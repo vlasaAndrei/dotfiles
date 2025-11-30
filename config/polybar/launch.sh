6979cb340c68faf3757175e2d59b885f48062f0f
vlasaAndrei <vlasa.andrei@agilefreaks.com>
2025-11-30 21:16:03 +0200

    Updated polybar launch script to have a delay so that the resolution of the monitor is loaded before topbars are added

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 killall -q polybar
+
+# Wait for displays to be configured
+sleep 2
+
+# Wait for all polybar instances to terminate
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
+
+# Launch polybar for each monitor
 polybar topbar-hdmi &
 polybar topbar-edp &
 
